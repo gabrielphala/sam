266b79b874fbbb5bb75f1392cea0db12a2d9f10a
gabrielphala <gabrielphala@outlook.com>
2023-05-03 00:07:09 +0200

    Re configured database

--- src/config/database.js
+++ src/config/database.js
@@ -2,5 +2,5 @@ const { Connection } = require('sqlifier')
 
 const conn = new Connection();
 
-conn.createConnection('localhost', 'root', '', require('mysql'))
+conn.createConnection('localhost', 'root', process.env.NODE_ENV == 'development' ? '' : process.env.ROOT_PASS, require('mysql'))
 conn.createDatabase('sam')
