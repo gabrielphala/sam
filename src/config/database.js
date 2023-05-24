214f66cf8afbb71e42f2350c0580cbb08e056688
gabrielphala <gabrielphala@outlook.com>
2023-05-24 19:25:28 +0200

    updated database config

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,7 @@ const { Connection } = require('sqlifier')
 
 const conn = new Connection();
 
-conn.createConnection('localhost', 'root', process.env.NODE_ENV == 'development' ? '' : process.env.ROOT_PASS, require('mysql'))
+conn.createConnection('127.0.0.1', 'root', process.env.NODE_ENV == 'development' ? '' : process.env.ROOT_PASS, require('mysql'))
 conn.createDatabase('sam')
 
 require("../models/init");
